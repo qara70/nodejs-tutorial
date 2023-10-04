a95271eb03f8aa45aafc7bf7fa751753153fe07f
qara70 <entsuka70@gmail.com>
2023-10-05 08:32:03 +0900

    feat: add /api

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     }
 );
 
+app.get("/api", (req, res) => {
+    res.status(200).json({ message: "Hello from server!" });
+    }
+);
+
 app.listen(3000, () =>
     console.log("Server listening on port 3000")
 );
